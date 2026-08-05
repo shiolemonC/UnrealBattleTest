2d9da6e5baa2bedf6979f379133f786df6f10b20
shiolemonC <haganechen@gmail.com>
2026-08-05 17:14:33 +0900

    Add UE 5.5 C++ third-person project

---
+++ Source/BattleTest/BattleTestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABattleTestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABattleTestGameMode();
+};
+
+
+
